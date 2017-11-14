0b38c4c2c5e69fa15c45358c42bdb0223af1365b
Jozef Lukac <jozef.lukac@globallogic.com>
2017-11-14 14:12:04 +0100

    Express js server initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
-const path = require('path');
+var webpack = require('webpack');
+var path = require('path');
+var debug = require('debug');
+// var proxy = require('http-proxy-middleware');
 
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+var config = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: APP_DIR + '/index.jsx',
   output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    path: BUILD_DIR,
+    filename: "client.min.js"
   },
-  module: {
-    rules: [
+
+  module : {
+    loaders : [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-    ],
+        test: /\.jsx?$/,
+        include : APP_DIR,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
   },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
+  devServer: {
+    host: 'localhost', // Defaults to `localhost`
+    port: 3000, // Defaults to 8080
+    proxy: {
+      '^/api/*': {
+        target: 'http://localhost:3000/',
+        secure: false
+      }
+    }
   },
-};
+
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+}
+
+module.exports = config;
