d51cf22ee019123057ffaa23df1a4a5ec2853948
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2017-11-19 17:05:55 +0100

    Edited intro page

---
+++ src/app/pages/WelcomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render} from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Default from '../layouts/Default.jsx';
+import SideBar from '../components/SideBar.jsx';
+import EventsList from '../components/EventsList.jsx';
+import Login from '../components/Login.jsx';
+
+export default class WelcomePage extends React.Component {
+  render() {
+    return (<MuiThemeProvider>
+      <div>
+        <Default/>
+        <SideBar/>
+        <EventsList/>
+      <Login />
+      </div>
+    </MuiThemeProvider>);
+  }
+}
