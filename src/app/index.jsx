9b6f22e2bede675b11f929d9ba1fb4d8e22af78b
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2017-12-07 19:18:49 +0100

    Edited routes
    Edited routes between pages

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,11 +3,15 @@ import {render} from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import WelcomePage from './pages/WelcomePage.jsx';
+import UserDash from './pages/UserDash.jsx';
+import AdminDash from './pages/AdminDash.jsx';
 
 render((
   <Router>
     <Switch>
       <Route exact path="/" component={WelcomePage}/>
+    <Route path='/UserDash' component={UserDash}/>
+  <Route path='/AdminDash' component={AdminDash}/>
     </Switch>
   </Router>
   ),
